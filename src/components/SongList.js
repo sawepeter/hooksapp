fd5134e651f9a32320c7d420a2c9a3c43fc3b5de
sawepeter <sawepeter6@gmail.com>
2023-04-04 01:31:09 +0300

    hooks playground

---
+++ src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>This wild darkness</li>
+                <li>Memory gospel</li>
+            </ul>
+        </div>
+    );
+}
+
+export default SongList;
