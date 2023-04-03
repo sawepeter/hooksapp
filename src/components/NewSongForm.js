bb123b6cf6b57eaf27eff74b4728a28215bfddf2
sawepeter <sawepeter6@gmail.com>
2023-04-04 02:19:02 +0300

    React js Forms

---
+++ src/components/NewSongForm.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const NewSongForm = ({ addSong }) => {
+   const [title, setTitle ] = useState('');
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    addSong(title);
+    setTitle('');
+    console.log(title);
+   }
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <label>Song name:</label>
+            <input type="text" value={title} required onChange ={(e) => setTitle(e.target.value)} />
+            <input type="submit" value="Add song"/>
+        </form>
+     );
+}
+ 
+export default NewSongForm;
